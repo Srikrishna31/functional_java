f258b1d7eb4c180946197763dcf0c36c51147a61
Krishna Addepalli <coolkrishna31@gmail.com>
2021-11-22 23:33:05 +0530

    Add a simple Tuple class.

---
+++ src/main/java/com/functional/Tuple.java
@@ -0,0 +1,11 @@
+package com.functional;
+
+public class Tuple<T,U> {
+    public final T _1;
+    public final U _2;
+
+    public Tuple(T t, U u) {
+        this._1 = t;
+        this._2 = u;
+    }
+}
