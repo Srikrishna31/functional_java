07caeaca73dc88ec64e22c911bb42d8166a05fe0
Krishna Addepalli <coolkrishna31@gmail.com>
2022-01-05 13:36:08 +0100

    Add methods set, get and modify that allow modifying a state object,
    without considering the output values.

---
+++ src/main/java/com/state/Nothing.java
@@ -0,0 +1,11 @@
+package com.state;
+
+/**
+ * A class to represent a Nothing type, which can be useful to represent a
+ * noop or nothing type.
+ */
+public final class Nothing {
+    private Nothing() {}
+
+    public static final Nothing instance = new Nothing();
+}
--- src/test/java/com/state/StateTest.java
+++ src/test/java/com/state/StateTest.java
@@ -2,9 +2,31 @@ package com.state;
 
 import org.junit.Test;
 
+class Point {
+    public final int x;
+    public final int y;
+    public final int z;
+
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Point(%s, %s, %s)", x, y, z);
+    }
+}
 public class StateTest {
     @Test
     public void testState() {
+        var ns = Random.integer.flatMap(x ->
+                                        Random.integer.flatMap(y ->
+                                                Random.integer.map(z ->
+                                                        new Point(x,y,z))));
+        var pt = ns.run.apply(JavaRNG.rng(0))._1;
 
+        System.out.println(pt);
     }
 }
